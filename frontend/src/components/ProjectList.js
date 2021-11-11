5d5130fe5c2c46774027d49581bf098d539d61ad
alvinlees <lxw97@hotmail.com>
2021-11-11 18:00:32 +0800

    project update

--- frontend/src/components/ProjectList.js
+++ frontend/src/components/ProjectList.js
@@ -1,15 +1,13 @@
-import React from "react";
-import Project from "./Project"
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-
-const data =
-[
+const data = [
   {
-    "id": 1,
-    "user_id": 4,
-    "name": "RTF",
-    "budget": 12000,
-    "description": "Realtime Face Recogniton"
+    id: 1,
+    user_id: 4,
+    name: "RTF",
+    budget: 12000,
+    description: "Realtime Face Recogniton",
   },
   // {
   //   "id": 2,
@@ -25,14 +23,36 @@ const data =
   //   "budget": 11000,
   //   "description": "Upgrade Legacy System"
   // }
-]
+];
 
 const ProjectList = () => {
   //Replace this after getting the API
-  const projects = data.map(json => <Project result={json} />)
-  return <div>
-    {projects}
-  </div>;
+  //const projects = result.map(json => <Project result={json} />)
+  const navigate = useNavigate();
+
+  const handleList = (e) => {
+    e.preventDefault();
+    console.log("success");
+    navigate("/Project");
+  };
+
+  return (
+    <div>
+      <h1>All Projects</h1>
+      {data.map((item) => {
+        const { id, user_id, name, budget, description } = item;
+        return (
+          <section>
+            <h1>
+              {id}, {name}, {budget}
+            </h1>
+            <p>{description}</p>
+            <button onClick={handleList}>Details</button>
+          </section>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ProjectList;
